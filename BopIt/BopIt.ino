f3f42adba1c35868b4d9f1d4fd7d63056c3ac340
Maya Hegde <mah451@pitt.edu>
2022-07-02 17:22:33 -0400

    Commented final prototype code

--- BopIt/BopIt.ino
+++ BopIt/BopIt.ino
@@ -14,10 +14,6 @@ void setup() {
   lcd.init();
   lcd.backlight();
 
-  // Initialize Interrupt
-//  Timer1.initialize(1000000); 
-//  Timer1.attachInterrupt(InterruptHandler);
-
   // set up pins
   pinSetup();
 
@@ -28,18 +24,12 @@ void loop() {
   if (first == 1) { // if this is the first pass of the game...
     ChooseTask(); // choose the first task
     first = 0; // change flag
-    second = 1;
   }
-
-  else if (timeOut == 1) {
-    inputHandler(false); // failure
-    timeOut == 0;
-  }
-
+  
   PollInput(); // otherwise, poll for user input
   
-  count = count - 1;
-  if (count == 0) {
+  count = count - 1; // decrement the counter for user response time
+  if (count == 0) { // if the counter is 0...
     inputHandler(false); // failure
   }
 }
--- BopIt/PollInput.ino
+++ BopIt/PollInput.ino
@@ -1,23 +1,24 @@
 void PollInput() {
   // 1 = Strum, 2 = Fret, 3 = Whammy
-  int fretButtonState = digitalRead(Fret);
-  int whammyButtonState = analogRead(Whammy);
-  int Strum1ButtonState = digitalRead(StrumSw1);
-  int Strum2ButtonState = digitalRead(StrumSw2);
+  
+  int fretButtonState = digitalRead(Fret); // read from fret signal
+  int whammyButtonState = analogRead(Whammy); // read from whammy signal
+  int Strum1ButtonState = digitalRead(StrumSw1); // read from the first strum signal
+  int Strum2ButtonState = digitalRead(StrumSw2); // read from the second strum signal
     
-  if(Strum1ButtonState == LOW) {
-    inputHandler(currTask == 1);
+  if(Strum1ButtonState == LOW) { // if the first strum is triggered...
+    inputHandler(currTask == 1); // check if accurate
   }
 
-  if(Strum2ButtonState == LOW) {
-    inputHandler(currTask == 1);
+  if(Strum2ButtonState == LOW) { // if the second strum is triggered...
+    inputHandler(currTask == 1); // check if accurate
   }
 
-  if(fretButtonState == LOW) {
-    inputHandler(currTask == 2);
+  if(fretButtonState == LOW) { // if the fret is triggered...
+    inputHandler(currTask == 2); // check if accurate
   }
 
-  if(whammyButtonState < 500) {
-    inputHandler(currTask == 3);
+  if(whammyButtonState < 500) { // if the whammy is triggered...
+    inputHandler(currTask == 3); // check if accurate
   }
 }
